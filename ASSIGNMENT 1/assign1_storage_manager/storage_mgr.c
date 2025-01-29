2e273638a35bda51e591a256f019e07917b06622
AtharvPat <atharvpat03@gmail.com>
2025-01-29 16:52:00 -0600

    Updated Files

--- ASSIGNMENT 1/assign1_storage_manager/storage_mgr.c
+++ ASSIGNMENT 1/assign1_storage_manager/storage_mgr.c
@@ -9,34 +9,37 @@
 
 /* ***************************************************************************************** */
 
-void CHECK_FILE(char *File_Name)
+RC CHECK_FILE(char *File_Name)
 {
     // Checking if the file Exists
     if (File_Name== NULL) 
     {
     return RC_FILE_NOT_FOUND; // Returing Error Code
     }
+    return RC_OK;
 }
 
-void CHECK_FILE_HANDLE(SM_FileHandle *File_Handle)
+RC CHECK_FILE_HANDLE(SM_FileHandle *File_Handle)
 {
     if (File_Handle == NULL) {
     return RC_FILE_HANDLE_NOT_INIT; // Returing Error Code
   }
+  return RC_OK;
 }
 
-void CHECK_MEMORY_PAGE(SM_PageHandle Memory_Page)
+RC CHECK_MEMORY_PAGE(SM_PageHandle Memory_Page)
 {
-      f (Memeory_Page == NULL) {
+      if (Memory_Page == NULL) {
     return RC_WRITE_FAILED;
   }
+  return RC_OK;
 }
 
 /* ***************************************************************************************** */
 
 
 // Initialization Function of the Storage Manager 
-void initStorageMgr(void)
+void initStorageManager(void)
 {
     // Message indicating the starting of the storage manager 
     printf("Storage Manager Starting...\n");
@@ -79,7 +82,10 @@ CHECK_FILE_HANDLE(File_Handle);
 
 // Open File in read and write mode
 FILE *file_position = fopen(File_Name,"r+");
-CHECK_FILE(file_position);
+if (file_position == NULL)
+{
+  return RC_FILE_NOT_FOUND; // Return Error Code
+}
 
 // Finiding out end of the file and moving the file pointer there to calculate the size of the file 
 if (fseek(file_position, 0, SEEK_END) != 0) 
@@ -89,13 +95,39 @@ if (fseek(file_position, 0, SEEK_END) != 0)
 
 // Store file pointer in file handle for future use
 File_Handle->mgmtInfo = file_position;
-File_Handle->fileName = File_Name;
+File_Handle->File_Name = File_Name;
 File_Handle->curPagePos = 0;
 
   // Count the number of Pages
 long fileSize = ftell(file_position);
 File_Handle->totalNumPages = (int) (fileSize / PAGE_SIZE);
 return RC_OK;
+}
+
+/* ***************************************************************************************** */
+
+RC closePageFile(SM_FileHandle *File_Handle) {
+  // check file handle
+  CHECK_FILE_HANDLE(File_Handle);
+
+  // retrieve file pointer
+  FILE *file_position = File_Handle->mgmtInfo;
+  fclose(file_position);
+  file_position = NULL;
+  return RC_OK;
+}
+
+/* ***************************************************************************************** */
+RC destroyPageFile(char *File_Name) {
+    // Validate the file name using the CHECK_FILE helper function
+    CHECK_FILE(File_Name);
+
+    // Attempt to remove the file
+    if (remove(File_Name) != 0) {
+        return RC_FILE_NOT_FOUND; // Return error code if file removal fails
+    }
+
+    return RC_OK; // Return success code if file removal is successful
 }
 
 /* ***************************************************************************************** */
@@ -115,8 +147,10 @@ CHECK_MEMORY_PAGE(Memory_Page);
 
 // Get file pointer from the file handle
 FILE *file_position = File_Handle->mgmtInfo;
-CHECK_FILE(file_position);
-
+if (file_position == NULL)
+{
+  return RC_FILE_NOT_FOUND; // Return Error Code
+}
 // Calculate byte offset of the Page
  int byteOffset = PG_Num * PAGE_SIZE;
  if (fseek(file_position, byteOffset, SEEK_SET) != 0) {
@@ -134,10 +168,36 @@ CHECK_FILE(file_position);
 
 /* ***************************************************************************************** */
 
+int getBlockPos(SM_FileHandle *File_Handle) {
+  // check parameters
+  return (File_Handle == NULL) ? -1 : File_Handle->curPagePos;
+}
+
+/* ***************************************************************************************** */
+
+RC readFirstBlock(SM_FileHandle *File_Handle, SM_PageHandle Memory_Page) {
+  return readBlock(0, File_Handle, Memory_Page);
+} 
+
+/* ***************************************************************************************** */
+
+RC readPreviousBlock(SM_FileHandle *File_Handle, SM_PageHandle Memory_Page) {
+  // if the value of position is -1, errors happened in this process
+  int PG_Num = getBlockPos(File_Handle);
+  if (PG_Num == -1) {
+    return RC_FILE_HANDLE_NOT_INIT;
+  }
+
+  // the previous block should be PG_Num - 1
+  return readBlock(PG_Num - 1, File_Handle, Memory_Page);
+}
+
+/* ***************************************************************************************** */
+
 RC readCurrentBlock(SM_FileHandle *File_Handle, SM_PageHandle Memory_Page) {
 
 // get the current page position using the getBlockPos() function
-  int PG_Num = getBlockPos(fHandle);
+  int PG_Num = getBlockPos(File_Handle);
 
   //If the the retrived page number is -1 then return an error
   if (PG_Num== -1) {
@@ -151,14 +211,42 @@ RC readCurrentBlock(SM_FileHandle *File_Handle, SM_PageHandle Memory_Page) {
 
 /* ***************************************************************************************** */
 
+RC readNextBlock(SM_FileHandle *File_Handle, SM_PageHandle Memory_Page) {
+  // errors happen if PG_Num is not positive
+  int PG_Num = getBlockPos(File_Handle);
+  if (PG_Num == -1) {
+    return RC_FILE_HANDLE_NOT_INIT;
+  }
 
-RC writeBlock(int PG_Num, SM_FileHandle *File_Handle, SM_PageHandle Memeory_Page) {
+  // the next block should be PG_Num + 1
+  return readBlock(PG_Num + 1, File_Handle, Memory_Page);
+}
+
+/* ***************************************************************************************** */
+
+RC readLastBlock(SM_FileHandle *File_Handle, SM_PageHandle Memory_Page) {
+    // Validate the file handle using the CHECK_FILE_HANDLE helper function
+    CHECK_FILE_HANDLE(File_Handle);
+
+    // Validate the memory page using the CHECK_MEMORY_PAGE helper function
+    CHECK_MEMORY_PAGE(Memory_Page);
+
+    // Calculate the last block page number
+    int lastBlockPG_Num = File_Handle->totalNumPages - 1;
+
+    // Read the last block
+    return readBlock(lastBlockPG_Num, File_Handle, Memory_Page);
+}
+
+/* ***************************************************************************************** */
+
+RC writeBlock(int PG_Num, SM_FileHandle *File_Handle, SM_PageHandle Memory_Page) {
 
 // Checking if the file handle is Vaid 
-CHECK_FileHandle(File_Handle);
+CHECK_FILE_HANDLE(File_Handle);
 
 // Check if the Memory pointer is not NUll 
-CHECK_MEMORY_PAGE(Memeory_Page);
+CHECK_MEMORY_PAGE(Memory_Page);
 
 // Check if the requested file is in the bounds
 if (PG_Num < 0 || PG_Num >= File_Handle->totalNumPages) {
@@ -180,13 +268,84 @@ int ByteOffSet = PG_Num * PAGE_SIZE;
   }
 
 // Write the data from the memory into the file
-  if (fwrite(Memory_Page, sizeof(char), strlen(memPage), file_position) < PAGE_SIZE) {
+  if (fwrite(Memory_Page, sizeof(char), strlen(Memory_Page), file_position) < PAGE_SIZE) {
 
     return RC_WRITE_FAILED; // Return Faile Code if Failed 
   }
-  File_Handle->curPagePos = Page_Num;
+  File_Handle->curPagePos = PG_Num;
   return RC_OK;  // Return Sucess Code
 
 }
 
 /* ***************************************************************************************** */
+
+RC writeCurrentBlock(SM_FileHandle *File_Handle, SM_PageHandle Memory_Page) {
+    // Validate the file handle using the CHECK_FILE_HANDLE helper function
+    CHECK_FILE_HANDLE(File_Handle);
+
+    // Validate the memory page using the CHECK_MEMORY_PAGE helper function
+    CHECK_MEMORY_PAGE(Memory_Page);
+
+    // Write the block using the current page number
+    int curPagePos = File_Handle->curPagePos;
+    return writeBlock(curPagePos, File_Handle, Memory_Page);
+}
+
+/* ***************************************************************************************** */
+
+
+RC appendEmptyBlock(SM_FileHandle *File_Handle) {
+  // check file handle
+  CHECK_FILE_HANDLE(File_Handle);
+
+  // this is to get non-empty file
+  FILE *file_position = File_Handle->mgmtInfo;
+  if (file_position == NULL) {
+    return RC_FILE_NOT_FOUND;
+  }
+
+  // this will move pointer to the end
+  if (fseek(file_position, 0, SEEK_END) != 0) {
+    return RC_READ_NON_EXISTING_PAGE;
+  }
+
+  // allocate page size and assign value to string, return rc write failed
+  char *temp= (char *) calloc(PAGE_SIZE, sizeof(char)); // Allocate on the stack and initialize to zero
+  if (fwrite(temp, sizeof(char), PAGE_SIZE, file_position) < PAGE_SIZE) {
+    return RC_WRITE_FAILED;
+  }
+
+  // add 1 to total pages and free the memory
+  File_Handle->totalNumPages = File_Handle->totalNumPages + 1;
+  free(temp);
+  return RC_OK;
+}
+
+/* ***************************************************************************************** */
+
+RC ensureCapacity(int numOf_PG, SM_FileHandle *File_Handle) {
+    // Validate the file handle using the CHECK_FILE_HANDLE helper function
+    CHECK_FILE_HANDLE(File_Handle);
+
+    // Validate the number of pages (ensure it is at least 1)
+    if (numOf_PG < 1) {
+        return RC_READ_NON_EXISTING_PAGE;
+    }
+
+    // Append remaining blocks if the current number of pages is less than required
+    int current_NumberOf_PG = File_Handle->totalNumPages;
+    int count = numOf_PG - current_NumberOf_PG;
+
+    int i = 0; // Initialize the counter
+    while (i < count) {
+    appendEmptyBlock(File_Handle);
+    i++; // Increment the counter
+    }
+
+    // Verify if the total number of pages matches the required number
+    if (File_Handle->totalNumPages != numOf_PG) {
+        return RC_WRITE_FAILED;
+    }
+
+    return RC_OK;
+}
